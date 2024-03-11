7944e7645c3daedad065a3c765ff98a4eb79e844
akshatt20 <22bec016@nith.ac.in>
2024-03-11 19:45:52 +0530

    first commit

---
+++ app/src/main/java/com/example/projectadd/LogInDoctorActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projectadd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogInDoctorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in_doctor)
+    }
+}
---
+++ app/src/main/java/com/example/projectadd/SignUpDoctorActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projectadd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpDoctorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_doctor)
+    }
+}
