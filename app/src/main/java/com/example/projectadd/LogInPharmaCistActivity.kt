043c182ade53b6a41af5ef72e67f78437953f7b9
akshatt20 <22bec016@nith.ac.in>
2024-03-11 22:48:30 +0530

    second commit

---
+++ app/src/main/java/com/example/projectadd/LogInPharmaCistActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projectadd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogInPharmaCistActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in_pharma_cist)
+    }
+}
